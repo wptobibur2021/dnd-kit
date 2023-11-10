785e134b3f4c9c40429486caf7019557a5807f2b
wptobibur2021 <wpdevelopment20@gmail.com>
2023-11-10 08:53:06 +0600

    Update responsive

--- app/page.tsx
+++ app/page.tsx
@@ -31,13 +31,12 @@ export default function Home() {
       setProducts((items) => {
         const oldIndex = items.map((item) => item.id).indexOf(active.id);
         const newIndex = items.map((item) => item.id).indexOf(over.id);
-
         return arrayMove(items, oldIndex, newIndex);
       });
     }
   };
   return (
-    <div className="container m-40 ">
+    <div className="container sm:m-10 xl:m-40 ">
       <div className="bg-white rounded-lg m-auto">
         <div className="border-b border-neutral-300 px-10 pb-2 pt-5">
           <div>
@@ -47,7 +46,7 @@ export default function Home() {
         <div className="p-10">
           <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
             <SortableContext items={products}>
-              <div className="grid grid-cols-8 gap-10">
+              <div className="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 grid-cols-2 gap-5 md:gap-10">
                 {products.map((product, index) => (
                   <Product product={product} key={product.id} index={index} />
                 ))}
